83b768aed9e30534584e92faea5b79e56535cd6b
Péter <peter@perenyi.net>
2024-01-04 08:28:11 +1100

    Create loud_setup.sh

---
+++ files/usr/bin/loud_setup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo Loud config utility
+read -p "Host name: " LOUD_HOSTNAME
+read -p "Wifi SSID: " LOUD_WIFI_SSID
+read -p "Wifi Key: " LOUD_WIFI_KEY
+
+uci set system.@system[0].hostname='$LOUD_HOSTNAME'
+
+uci set network.lan=interface
+uci set network.lan.proto='dhcp'
+
+uci del wireless.default_radio0
+
+uci set wireless.wifinet1=wifi-iface
+uci set wireless.wifinet1.device='radio0'
+uci set wireless.wifinet1.mode='sta'
+uci set wireless.wifinet1.network='lan'
+uci set wireless.wifinet1.encryption='psk2'
+uci set wireless.wifinet1.ssid='$LOUD_WIFI_SSID'
+uci set wireless.wifinet1.key='$LOUD_WIFI_KEY'
+
+#uci set librespot.librespot.device_name='$LOUD_HOSTNAME'
+
+uci commit network
+uci commit wireless
+uci commit system
+
+#uci commit librespot
+
+/etc/init.d/system reload
+/etc/init.d/network reload
+/etc/init.d/wireless reload
+#/etc/init.d/librespot restart
+
+reboot
