a852d89965e003bad6b6c9a68c64de85bb3a8ef7
Péter <peter@perenyi.net>
2024-01-05 10:14:28 +0000

    Fix setup file

--- files/usr/bin/loud_setup.sh
+++ files/usr/bin/loud_setup.sh
@@ -7,15 +7,15 @@ read -p "Wifi Key: " LOUD_WIFI_KEY
 
 uci set system.@system[0].hostname="$LOUD_HOSTNAME"
 
-uci set network.lan=interface
-uci set network.lan.proto='dhcp'
+uci set network.wlan=interface
+uci set network.wlan.proto='dhcp'
 
 uci del wireless.default_radio0
 
 uci set wireless.wifinet1=wifi-iface
 uci set wireless.wifinet1.device='radio0'
 uci set wireless.wifinet1.mode='sta'
-uci set wireless.wifinet1.network='lan'
+uci set wireless.wifinet1.network='wlan'
 uci set wireless.wifinet1.encryption='psk2'
 uci set wireless.wifinet1.ssid="$LOUD_WIFI_SSID"
 uci set wireless.wifinet1.key="$LOUD_WIFI_KEY"
